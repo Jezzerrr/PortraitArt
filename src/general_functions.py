ef2569b6e03c0f5af9fa904668ed063ed8b34b05
Jesse Metz <j.metz@technischeunie.nl>
2026-06-11 13:11:48 +0200

    initial commit

---
+++ src/general_functions.py
@@ -0,0 +1,61 @@
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+from scipy.stats import linregress
+from datetime import datetime
+import random
+import os
+import seaborn as sns
+
+from PIL import Image
+
+
+def extract_square(input_image_path, target_size=False, move_vertical=1, move_horizontal=1, zoom=1):
+    """Crop a square region from an image with optional zoom and directional offset. Optionally resize to a target size."""
+    # Load the original image
+    img = Image.open(input_image_path)
+
+    # Determine the size to create a square image
+    min_dimension = min(img.width, img.height) * zoom
+
+    # Calculate the coordinates for cropping to get the middle part
+    left   = (img.width  * move_horizontal - min_dimension) // 2
+    right  = (img.width  * move_horizontal + min_dimension) // 2
+    top    = (img.height * move_vertical   - min_dimension) // 2
+    bottom = (img.height * move_vertical   + min_dimension) // 2
+
+    # Crop the image
+    img_cropped = img.crop((left, top, right, bottom))
+    if target_size:
+        img_cropped = decrease_image_size(img_cropped, target_size)
+
+    return img_cropped
+
+
+def decrease_image_size(img, target_size):
+    """Resize an image to a specified square dimension using high-quality resampling."""
+    # if target_size >= img.width or target_size >= img.height:
+    #     raise ValueError("Target size should be smaller than the original size.")
+
+    img_resized = img.resize((target_size, target_size), Image.Resampling.LANCZOS)
+    return img_resized
+
+
+def save_image_with_unique_name(img, output_base_path='output/output_image.jpg'):
+    """Save an image to disk with a unique filename to avoid overwriting existing files."""
+    # Check if the file exists and create a unique name
+    output_image_path = create_unique_file_name(output_base_path)
+
+    # Save the image
+    img.save(output_image_path)
+    return output_image_path
+
+
+def create_unique_file_name(output_base_path):
+    """Generate a unique file path by appending a counter if the base path already exists."""
+    count = 0
+    output_image_path = output_base_path
+    while os.path.exists(output_image_path):
+        count += 1
+        output_image_path = f"{output_base_path.rsplit('.', 1)[0]}_{count}.{output_base_path.rsplit('.', 1)[1]}"
+    return output_image_path
